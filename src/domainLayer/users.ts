69016bfbc5ff1076c8f193fd6b14661f365d5b96
shabil15 <mohammedshabil15@gmail.com>
2024-04-18 13:09:45 +0530

    eslint for code linting

--- src/domainLayer/users.ts
+++ src/domainLayer/users.ts
@@ -1,12 +1,1 @@
-export default interface Student{
-  FCMToken?: any
-  _id?: string,
-  id?:string
-  username: string
-  email: string,
-  mobile?: string,
-  password: string,
-  image: string,
-  premium?: boolean,
-  token?:string
-}
+
